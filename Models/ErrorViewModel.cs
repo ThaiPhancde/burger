9bb7106e98145350d07f60fc248b7fd64a3fa7ae
ThaiPhancde <anthaiphanxuan@gmail.com>
2024-04-26 09:54:52 +0700

    khoi tao du an

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace burger.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Models/tblMenu.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+namespace burger.Models
+{
+        [Table("tblMenu")]
+        public class tblMenu
+        {
+            [Key]
+            public int MenuID { get; set; }
+            public string? MenuNameP{ get; set; }
+            public bool? IsActive{  get; set; }
+            public string? ControllerName{ get; set; }
+            public string? ActionName{ get; set; }
+            public int Levels{  get; set; }
+            public int ParentID{ get; set; }
+            public string? Link{ get; set; }
+            public int MenuOrder{get; set; }
+            public int Position{ get; set; }
+        }
+}

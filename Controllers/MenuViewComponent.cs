34bf8976ce6382a81eece57f41655c23dc8b27de
ThaiPhancde <anthaiphanxuan@gmail.com>
2024-04-26 15:38:02 +0700

    Sửa menu chính

--- Controllers/MenuViewComponent.cs
+++ Controllers/MenuViewComponent.cs
@@ -2,18 +2,21 @@ using burger.Models;
 using Microsoft.AspNetCore.Mvc;
 namespace burger.Controllers
 {
-        [ViewComponent(Name = "MenuView")]
-        public class MenuViewComponent : ViewComponent{
-            private readonly DataContext _context;
-            public MenuViewComponent(DataContext context){
-                _context = context;
+    [ViewComponent(Name = "MenuView")]
+    public class MenuViewComponent : ViewComponent
+    {
+        private readonly DataContext _context;
+        public MenuViewComponent(DataContext context)
+        {
+            _context = context;
 
-            }
-            public async Task<IViewComponentResult> InvokeAsync(){
-                var listOfMenu = (from m in _context.Menu
-                                where(m.IsActive == true) && (m.Position == 1)
-                                select m).ToList();
-                return await Task.FromResult((IViewComponentResult)View("Default", listOfMenu));
-            }
         }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var listOfMenu = (from m in _context.Menu
+                              where (m.IsActive == true) && (m.Position == 1)
+                              select m).ToList();
+            return await Task.FromResult((IViewComponentResult)View("Default", listOfMenu));
+        }
+    }
 }
